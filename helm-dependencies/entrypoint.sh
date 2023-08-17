85bb1e4f27abdacd4402042a80cc0f39664ec22d
Artem Lajko <artem.lajko@hpa.hamburg.de>
2023-08-17 19:20:21 +0200

    debug

--- helm-dependencies/entrypoint.sh
+++ helm-dependencies/entrypoint.sh
@@ -20,114 +20,114 @@ function readFile() {
     echo "DEBUG: file"
     echo $file
     echo "DEBUG: Config_Path"
-    echo ${CONFIG_PATH}
+    echo "${CONFIG_PATH}"
 
     # Get the number of dependencies
-    count=$(yq e '.dependencies | length' $file)
+    # count=$(yq e '.dependencies | length' $file)
 
 }
 
-function checkHelmDependenciesAndUpdateGitHub() {
+# function checkHelmDependenciesAndUpdateGitHub() {
 
-    # Iterate over the list
-    for ((i = 0; i < $count; i++)); do
-        # Name of the dependency like External DNS
-        name=$(yq e ".dependencies[$i].name" $file)
-        # Path to the Chart.yaml file
-        chart_file=$(yq e ".dependencies[$i].source.file" $file)
-        # Path to the version number in the Chart.yaml file like 6.20.0
-        version_path=$(yq e ".dependencies[$i].source.path" $file)
-        # Repository name for the Artifact API
-        repo_name=$(yq e ".dependencies[$i].repository.name" $file)
-        # Repository url for the Artifact API
-        repo_url_path=$(yq e ".dependencies[$i].repository.path" $file)
+#     # Iterate over the list
+#     for ((i = 0; i < $count; i++)); do
+#         # Name of the dependency like External DNS
+#         name=$(yq e ".dependencies[$i].name" $file)
+#         # Path to the Chart.yaml file
+#         chart_file=$(yq e ".dependencies[$i].source.file" $file)
+#         # Path to the version number in the Chart.yaml file like 6.20.0
+#         version_path=$(yq e ".dependencies[$i].source.path" $file)
+#         # Repository name for the Artifact API
+#         repo_name=$(yq e ".dependencies[$i].repository.name" $file)
+#         # Repository url for the Artifact API
+#         repo_url_path=$(yq e ".dependencies[$i].repository.path" $file)
 
-        # Sanitize the repo name
-        sanitized_name=$(echo $repo_name | cut -d'/' -f1)
+#         # Sanitize the repo name
+#         sanitized_name=$(echo $repo_name | cut -d'/' -f1)
 
-        #Change directory to the chart file directory
-        cd $(dirname $chart_file) || exit
+#         #Change directory to the chart file directory
+#         cd $(dirname $chart_file) || exit
 
-        # Read the version from the Chart.yaml file
-        version=$(yq e "$version_path" "$(basename $chart_file)")
-        repo_url=$(yq e "$repo_url_path" "$(basename $chart_file)")
+#         # Read the version from the Chart.yaml file
+#         version=$(yq e "$version_path" "$(basename $chart_file)")
+#         repo_url=$(yq e "$repo_url_path" "$(basename $chart_file)")
 
-        # Add the repo to helm
-        helm repo add $sanitized_name $repo_url || true
-        helm repo update 1 &>/dev/null || true
+#         # Add the repo to helm
+#         helm repo add $sanitized_name $repo_url || true
+#         helm repo update 1 &>/dev/null || true
 
-        #Get the current version with the Artifact API
-        current_version=$(helm search repo $repo_name --output yaml | yq eval '.[0].version')
+#         #Get the current version with the Artifact API
+#         current_version=$(helm search repo $repo_name --output yaml | yq eval '.[0].version')
 
-        # Output
-        echo "Name: $name"
-        echo "Version in Chart.yaml: $version"
-        echo "Current Version: $current_version"
+#         # Output
+#         echo "Name: $name"
+#         echo "Version in Chart.yaml: $version"
+#         echo "Current Version: $current_version"
 
-        # If there's a difference between the versions
-        if [ "$version" != "$current_version" ]; then
-            if [ ! $(git branch --list update-helm-$sanitized_name-$current_version) ]; then
-                echo "There's a difference between the versions."
+#         # If there's a difference between the versions
+#         if [ "$version" != "$current_version" ]; then
+#             if [ ! $(git branch --list update-helm-$sanitized_name-$current_version) ]; then
+#                 echo "There's a difference between the versions."
 
-                # Get values from the repo
-                values=$(helm show values $repo_name --version $version)
-                echo "$values" >values.yaml
-                current_values=$(helm show values $repo_name --version $current_version)
-                echo "$current_values" >current_values.yaml
+#                 # Get values from the repo
+#                 values=$(helm show values $repo_name --version $version)
+#                 echo "$values" >values.yaml
+#                 current_values=$(helm show values $repo_name --version $current_version)
+#                 echo "$current_values" >current_values.yaml
 
-                diff_result=$(dyff between values.yaml current_values.yaml) || true
-                # Output differences
-                echo "$diff_result" >diff_result.txt
-                awk '{ printf "\t%s\n", $0 }' diff_result.txt >shift_diff_result.txt
-                shift_diff_result=$(cat shift_diff_result.txt)
+#                 diff_result=$(dyff between values.yaml current_values.yaml) || true
+#                 # Output differences
+#                 echo "$diff_result" >diff_result.txt
+#                 awk '{ printf "\t%s\n", $0 }' diff_result.txt >shift_diff_result.txt
+#                 shift_diff_result=$(cat shift_diff_result.txt)
 
-                # Delete the temporary files
-                rm values.yaml current_values.yaml diff_result.txt shift_diff_result.txt
+#                 # Delete the temporary files
+#                 rm values.yaml current_values.yaml diff_result.txt shift_diff_result.txt
 
-                # Configure git
-                git config --global user.email $PARAM_GIT_USER_EMAIL
-                git config --global user.name $PARAM_GIT_USER_NAME
+#                 # Configure git
+#                 git config --global user.email $PARAM_GIT_USER_EMAIL
+#                 git config --global user.name $PARAM_GIT_USER_NAME
 
-                # Replace the old version with the new version in the Chart.yaml file using sed
-                sed -i.bak "s/version: $version/version: $current_version/g" "$(basename $chart_file)" && rm "$(basename $chart_file).bak"
+#                 # Replace the old version with the new version in the Chart.yaml file using sed
+#                 sed -i.bak "s/version: $version/version: $current_version/g" "$(basename $chart_file)" && rm "$(basename $chart_file).bak"
 
-                # Create a new branch for this change
-                git checkout -b update-helm-$sanitized_name-$current_version
-                # Add the changes to the staging area
-                git add "$(basename $chart_file)"
+#                 # Create a new branch for this change
+#                 git checkout -b update-helm-$sanitized_name-$current_version
+#                 # Add the changes to the staging area
+#                 git add "$(basename $chart_file)"
 
-                # Create a commit with a message indicating the changes
-                git commit -m "Update $name version from $version to $current_version"
+#                 # Create a commit with a message indicating the changes
+#                 git commit -m "Update $name version from $version to $current_version"
 
-                # Push the new branch to GitHub
-                git push origin update-helm-$sanitized_name-$current_version
+#                 # Push the new branch to GitHub
+#                 git push origin update-helm-$sanitized_name-$current_version
 
-                # Create a GitHub Pull Request
-                gh pr create --title "Update $name version from $version to $current_version" --body "$shift_diff_result" --base main --head update-helm-$sanitized_name-$current_version || true
+#                 # Create a GitHub Pull Request
+#                 gh pr create --title "Update $name version from $version to $current_version" --body "$shift_diff_result" --base main --head update-helm-$sanitized_name-$current_version || true
 
-                # Get back to the source branch
-                git checkout $$PARAM_GIT_DEFAULT_BRANCH
+#                 # Get back to the source branch
+#                 git checkout $$PARAM_GIT_DEFAULT_BRANCH
 
-            else
-                echo "Branch already exists. Checking out to the existing branch." || true
-            fi
+#             else
+#                 echo "Branch already exists. Checking out to the existing branch." || true
+#             fi
 
-        else
-            echo "There's no difference between the versions."
-        fi
+#         else
+#             echo "There's no difference between the versions."
+#         fi
 
-        # Return to the original directory
-        cd - 1>/dev/null || exit
+#         # Return to the original directory
+#         cd - 1>/dev/null || exit
 
-        echo ""
-    done
+#         echo ""
+#     done
 
 }
 
 function start() {
     # Read the file
     readFile
-    checkHelmDependenciesAndUpdateGitHub
+    # checkHelmDependenciesAndUpdateGitHub
 }
 
 echo "[+] helm-dependencies"
